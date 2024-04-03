e554bb57cac7644a0776e632404e1d18658e09a4
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 22:33:03 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-}
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
 
-resource "aws_s3_bucket" "presentation_bucket" {
-  bucket = "riyaz-dist-bucket"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "presentation_bucket_versioning" {
-  bucket = aws_s3_bucket.presentation_bucket.bucket
-
-  versioning_configuration {
-    status = "Enabled"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
 
-resource "aws_iam_role" "riyaz_role" {
-  name               = "riyaz-dist-role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "lambda.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
-}
+resource "aws_s3_object" "frontend_files" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  key    = "*"
+  source = "${var.frontend_build_dir}/*"
 
-resource "null_resource" "sync_frontend_dist" {
-  triggers = {
-    always_run = timestamp()
-  }
+  content_type = filebase64sha256("${var.frontend_build_dir}/*")
 
-  provisioner "local-exec" {
-    command = "cd frontend && aws s3 sync dist s3://riyaz-dist-bucket/dist --region us-east-1"
-  }
+  depends_on = [aws_s3_bucket.frontend_bucket]
 }
