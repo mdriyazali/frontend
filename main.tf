9045aa1d7b5adf4b95eb7b2fa9110fc4f6b353e3
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 21:43:47 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,45 +3,46 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "presentation_bucket" {
-  bucket = "riyaz-tf-buckett"
+  bucket = "riyaz-tf-frontend-bucket"
 }
 
 resource "aws_s3_bucket_versioning" "presentation_bucket_versioning" {
-  bucket = aws_s3_bucket.presentation_bucket.bucket
+  bucket = aws_s3_bucket.presentation_bucket.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# IAM Role
-resource "aws_iam_role" "riyaz_role" {
-  name               = "riyaz-tf-rolee"
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [{
+resource "aws_iam_role" "frontend_role" {
+  name = "riyaz-frontend-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
       "Effect": "Allow",
       "Principal": {
-        "Service": "ec2.amazonaws.com"
+        "Service": "s3.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
-    }]
-  })
+    }
+  ]
+}
+EOF
 }
 
-# Attach S3 access policy to the IAM role
-resource "aws_iam_policy_attachment" "attach_s3_access_policy" {
-  name       = "s3-access-policy-attachment"
-  roles      = [aws_iam_role.riyaz_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+resource "aws_iam_policy_attachment" "s3_access_attachment" {
+  name       = "s3_access_attachment"
+  roles      = [aws_iam_role.frontend_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-# Execute local shell command to sync the dist directory to S3
 resource "null_resource" "sync_frontend_dist" {
   provisioner "local-exec" {
     command = <<EOF
       cd frontend
-      aws s3 sync dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist
+      aws s3 sync dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist --region=us-east-1
     EOF
   }
 }
