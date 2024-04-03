e5bc5a150304facbd10b3436401457a3f9602e1d
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 22:36:00 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
-resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_s3_object" "frontend_files" {
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "your-bucket-name"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_object" "frontend_files" {
+  for_each = fileset(".", "**/*")
+
   bucket = aws_s3_bucket.frontend_bucket.id
-  key    = "*"
-  source = "${var.frontend_build_dir}/*"
+  key    = each.key
+  source = each.key
 
-  content_type = filebase64sha256("${var.frontend_build_dir}/*")
-
-  depends_on = [aws_s3_bucket.frontend_bucket]
+  etag = filemd5(each.key)
 }
