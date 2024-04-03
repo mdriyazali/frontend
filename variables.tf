8c0f09b49d1738fab7736463735790fda40242d3
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 22:32:40 +0545

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,4 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "S3 bucket name"
-  type        = string
-  default     = "riyazkhannn-bucket"
-}
-
-variable "role_name" {
-  description = "IAM role name"
-  type        = string
-  default     = "riyazkhannn-role"
-}
-
-variable "distribution_enabled" {
-  description = "Flag to enable/disable the CloudFront distribution"
-  type        = bool
-  default     = true
-}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "bucket_name" {}
+variable "frontend_build_dir" {}
