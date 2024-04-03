f1919b8b2da38d2fedcea4c9c4bd6b0b497d7048
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-04 00:16:55 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Change to your desired region
 }
 
 resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = "your-bucket-name"
-  acl    = "public-read"
+  bucket = "riyaz-new" # Change to your bucket name
+  acl    = "public-read"      # Adjust permissions as needed
+
+  website {
+    index_document = "index.html"
+  }
 }
 
-resource "aws_s3_bucket_object" "frontend_files" {
-  for_each = fileset(".", "**/*")
-
-  bucket = aws_s3_bucket.frontend_bucket.id
-  key    = each.key
-  source = each.key
-
-  etag = filemd5(each.key)
+output "bucket_domain_name" {
+  value = aws_s3_bucket.frontend_bucket.website_endpoint
 }
