93518c4ae373a7fbf64cc0af2cd3cdefb9cafbb9
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 21:30:42 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "presentation_bucket" {
-  bucket = var.bucket_name
+  bucket = "riyajkhan-bucket"
 
   # Enable versioning for the bucket
   versioning {
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "presentation_bucket" {
 
 # IAM Role
 resource "aws_iam_role" "riyaz_role" {
-  name               = var.role_name
+  name               = "riyajrole-role"
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [{
@@ -26,12 +26,19 @@ resource "aws_iam_role" "riyaz_role" {
   })
 }
 
+# Attach S3 access policy to the IAM role
+resource "aws_iam_policy_attachment" "attach_s3_access_policy" {
+  name       = "s3-access-policy-attachment"
+  roles      = [aws_iam_role.riyaz_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
 # Execute local shell command to sync the dist directory to S3
 resource "null_resource" "sync_frontend_dist" {
   provisioner "local-exec" {
     command = <<EOF
       cd frontend
-      aws s3 sync frontend/dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist --region ${var.region}
+      aws s3 sync frontend/dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist
     EOF
   }
 }
