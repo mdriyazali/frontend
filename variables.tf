4c3a351aafedcbad4adc5a16a94c2086b063aeca
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 16:42:29 +0545

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "riyazgit-presentation-bucket"
+}
+
+variable "distribution_enabled" {
+  description = "Flag to enable/disable the CloudFront distribution"
+  type        = bool
+  default     = true
+}
