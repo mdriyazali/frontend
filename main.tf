5a054520d9171c7fb3bc8c600d88cb2696ed4d9e
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-03 21:27:27 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,30 +26,12 @@ resource "aws_iam_role" "riyaz_role" {
   })
 }
 
-resource "aws_iam_policy" "s3_access_policy" {
-  name        = "S3AccessPolicy"
-  description = "IAM policy for accessing S3 bucket"
-  policy      = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [{
-      "Effect": "Allow",
-      "Action": "s3:GetObject",
-      "Resource": "${aws_s3_bucket.presentation_bucket.arn}/*"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_s3_access_policy" {
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-  role       = aws_iam_role.riyaz_role.name
-}
-
 # Execute local shell command to sync the dist directory to S3
 resource "null_resource" "sync_frontend_dist" {
   provisioner "local-exec" {
     command = <<EOF
       cd frontend
-      aws s3 sync frontend/dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist
+      aws s3 sync frontend/dist s3://${aws_s3_bucket.presentation_bucket.bucket}/dist --region ${var.region}
     EOF
   }
 }
