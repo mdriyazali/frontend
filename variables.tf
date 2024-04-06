2bbc408ec47150fa71a1c44ffca9fcc673c073a9
mdriyazali <96767371+mdriyazali@users.noreply.github.com>
2024-04-06 12:11:53 +0545

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "The region in which resources will be created"
-  default     = "us-east-1"
-}
-
-variable "s3_dist_bucket" {
-  description = "The name of the S3 bucket to store the website content"
-  default     = "riyaz_khan-s3-frontend-bucket"
-}
